bcfe0b9391469b53adcaff215d239bf1101febfb
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-06-29 17:17:00 +0530

    addec global style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,138 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
 
 const Navbar = () => {
+
+  const Nav = styled.nav`
+    .navbar-list {
+      display: flex;
+      gap: 4.8rem;
+
+      li {
+        list-style: none;
+
+        .navbar-link {
+          &:link,
+          &:visited {
+            display: inline-block;
+            text-decoration: none;
+            font-size: 1.8rem;
+            text-transform: uppercase;
+            color: ${({ theme }) => theme.colors.black};
+            transition: color 0.3s linear;
+          }
+
+          &:hover,
+          &:active {
+            color: ${({ theme }) => theme.colors.helper};
+          }
+        }
+      }
+    }
+
+    .mobile-navbar-btn {
+      display: none;
+
+      .close-outline {
+        display: none;
+      }
+    }
+
+    .mobile-navbar-btn[name="close-outline"] {
+      display: none;
+    }
+
+    @media (max-width: ${({ theme }) => theme.media.mobile}) {
+      .mobile-navbar-btn {
+        display: inline-block;
+        z-index: 999;
+        border: ${({ theme }) => theme.colors.black};
+
+        .mobile-nav-icon {
+          font-size: 4.2rem;
+          color: ${({ theme }) => theme.colors.black};
+        }
+      }
+
+      /* hide the original nav menu  */
+      .navbar-list {
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: #fff;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        flex-direction: column;
+        text-align: center;
+
+        transform: translateX(100%);
+
+        visibility: hidden;
+        opacity: 0;
+
+        li {
+          .navbar-link {
+            &:link,
+            &:visited {
+              font-size: 4.2rem;
+            }
+
+            &:hover,
+            &:active {
+              color: ${({ theme }) => theme.colors.helper};
+            }
+          }
+        }
+      }
+
+      .active .mobile-nav-icon {
+        display: none;
+        font-size: 4.2rem;
+        position: absolute;
+        top: 3%;
+        right: 10%;
+        color: ${({ theme }) => theme.colors.black};
+        z-index: 9999;
+      }
+
+      .active .close-outline {
+        display: inline-block;
+      }
+
+      .active .navbar-list {
+        visibility: visible;
+        opacity: 1;
+        transform: translateX(0);
+        z-index: 999;
+      }
+    }
+  `;
+
+
   return (
-    <nav>
+    <Nav>
       <div className='menyIcon'>
         <ul className='navbar-list'>
           <li>
-            <NavLink to='/'>Home</NavLink>
+            <NavLink className='navbar-link' to='/'>Home</NavLink>
           </li>
           <li>
-            <NavLink to='/about'>About</NavLink>
+            <NavLink className='navbar-link' to='/about'>About</NavLink>
           </li>
           <li>
-            <NavLink to='/service'>Sercices</NavLink>
+            <NavLink className='navbar-link' to='/service'>Sercices</NavLink>
           </li>
           <li>
-            <NavLink to='/contact'>Contact</NavLink>
+            <NavLink className='navbar-link' to='/contact'>Contact</NavLink>
           </li>
         </ul>
       </div>
-    </nav>
+    </Nav>
   )
 }
 
